6eb26ce4b9ee9077eb4d852bcc733a82967eec02
Abinet505 <abinatmail@gmail.com>
2021-10-04 16:37:07 +0300

    printf

--- main.h
+++ main.h
@@ -7,16 +7,16 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_odh(char c, unsigned int num);
-int print_reverse(char *s);
-int print_unknown_spec(char c);
-int dec_to_hex(char c, unsigned int num);
-int print_pointer(void *p);
-int print_S(char *);
-int print_rot13(char *c);
-int print_binary(unsigned int num);
 int print_num(long int n);
-int dec_to_oct(unsigned int num);
 int print_string(char *str);
+int print_binary(unsigned int num);
+int print_unknown_spec(char c);
+int print_odh(char c, unsigned int num);
+int dec_to_oct(unsigned int num);
+int dec_to_hex(char c, unsigned int num);
+int print_S(char *);
+int print_reverse(char *s);
+int print_rot13(char *c);
+int print_pointer(void *p);
 
 #endif /* MAIN_H */
